9c2f74fa0f1e8cf5c4defebe4fa532a2e31f4b49
omarelouahdani <omarelouahdani7@gmail.com>
2021-02-12 10:47:41 +0100

    first push

---
+++ src/test/java/org/sid/getwayservice/GetwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.getwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetwayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
